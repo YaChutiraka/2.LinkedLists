42d6c514498153a41cff65a9928ae980c8e8647f
YaChutiraka <c.naweeya@gmail.com>
2016-05-12 15:59:43 -0700

    implement some methods

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,10 +1,14 @@
 public class DoublyLinkedList {
+	DoubleLink head;
 	DoubleLink curr;
+	DoubleLink last;
 	DoubleLink prevLink;
 	DoubleLink nextLink;
 	DoubleLink newNode;
 	
 	public boolean search(Object item){
+		curr=head;
+		
 		boolean status=false;
 		return status;
 	}
@@ -14,22 +18,35 @@ public class DoublyLinkedList {
 	}
 	
 	public void add(Object newItem){
-		
+		newNode=new DoubleLink(newItem);
+		if(head==null){
+			//No nodes in this LinkedList to begin with
+			newNode.setNext(null);
+			newNode.setPrecede(null);
+			head=newNode;
+		}else{
+			newNode.setNext(head);
+			newNode.setPrecede(null);
+			head.setPrecede(newNode);
+			head=newNode;
+		}
 	}
 	
 	public boolean insertBefore(Object newItem, Object location){
+		//TODO
 		return false;
 	}
 	
 	public boolean insertAfter(Object newItem, Object location){
+		//TODO
 		return false;
 	}
 	
 	public void appendToTail(Object newItem){
-		
+		//TODO
 	}
 	
 	public void ShowData(){
-		
+		//TODO
 	}
 }
